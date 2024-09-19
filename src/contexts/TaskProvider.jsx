5398fe90d34971260927deae73282da8a0a2674f
avinashchodavarapu <avinashiniiitdwd@gmail.com>
2024-09-19 21:16:04 +0530

    Added Task Provider and updated application to that

---
+++ src/contexts/TaskProvider.jsx
@@ -0,0 +1,17 @@
+import React, {useState, useContext, createContext} from 'react';
+import {v4} from 'uuid';
+
+const TaskContext = createContext();
+export const useTodos = () => useContext(TaskContext);
+
+// eslint-disable-next-line react/prop-types
+export default function TaskProvider({ children }) {
+    const [tasks, setTasks] = useState([]);
+
+    const addTask = (task) => setTasks([...tasks, {id: v4(), value: task, status: "todo"}]);
+    const setStatusTask = (id, status) => setTasks(tasks.map(task => task.id === id ? {...task, status: status} : task));
+    const deleteTask = (id) => setTasks(tasks.filter(task => task.id !== id))
+    return(
+        <TaskContext.Provider value={{ tasks, addTask, setStatusTask, deleteTask }}>{children}</TaskContext.Provider>
+    )
+}
